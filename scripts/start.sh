287f12577a6a1e5c9561c93e84125d32d024893a
Charlie Escoboza Garcia <ing.charlie.garcia@gmail.com>
2019-01-26 10:26:29 -0400

    Feature/docker (#4)
    * Improve local development
    
    * Add production docker config

---
+++ scripts/start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Build the container
+docker build -f ./docker/Dockerfile -t librarian-api .
+
+# Run the container
+docker run -p 8080:8080 -v $(pwd)://usr/src/app/ librarian-api
